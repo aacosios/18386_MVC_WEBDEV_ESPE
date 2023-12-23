4b599e3b39bf7384b278e7202764c622d357eb23
aacosios <aacosios@espe.edu.ec>
2023-12-22 20:23:46 -0500

    Updated

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const mysql = require('mysql');
-const myConnection = require ('express-myconnection');
-const app = express();
+import app from "./app.js";
+import { port } from "./config.js";
 
-// import routers
-const customerRoutes = require('./routes/customer');
-
-// settings
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// middlewares
-app.use(morgan('dev'));
-app.use (myConnection(mysql, {
-    host: 'localhost',
-    user: 'root',
-    password: 'androcker0812.',
-    port: 3306,
-    database: 'dbmysql',
-}, 'single'));
-
-// routes
-app.use('/', customerRoutes);
-
-// static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// starting server
-app.listen(app.get('port'), () => {
-    console.log('Corriendo en el puerto 3000');
-});
+app.listen(port);
+console.log(`server on port ${port}`);
